58454c7039b4537d01d4fd0fb1a314711f707217
abienias97 <abienias97@gmail.com>
2022-11-30 14:14:26 +0100

    Making parts of layout

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -2,16 +2,55 @@
 
 @section('content')
 
-<h1>{{$heading;}}</h1>
+<div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
 @if(count($listings) == 0)
     <p>No listings</p>
 @else
 @foreach($listings as $listing)
-     <h2><a href="/{{$listing['id']}}">{{$listing['title']}}</a></h2>
-     <p>{{$listing['description']}}</p>
+    <div class="bg-gray-50 border border-gray-200 rounded p-6">
+        <div class="flex">
+            <img
+                class="hidden w-48 mr-6 md:block"
+                src="{{ asset('images/no-image.png') }}"
+                alt=""
+            />
+            <div>
+                <h3 class="text-2xl">
+                    <a href="/{{ $listing->id }}">{{ $listing->title }}</a>
+                </h3>
+                <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
+                <ul class="flex">
+                    <li
+                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                    >
+                        <a href="#">Laravel</a>
+                    </li>
+                    <li
+                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                    >
+                        <a href="#">API</a>
+                    </li>
+                    <li
+                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                    >
+                        <a href="#">Backend</a>
+                    </li>
+                    <li
+                        class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
+                    >
+                        <a href="#">Vue</a>
+                    </li>
+                </ul>
+                <div class="text-lg mt-4">
+                    <i class="fa-solid fa-location-dot"></i>{{ $listing->location }}</div>
+            </div>
+        </div>
+    </div>
 @endforeach
 
 @endif
+
+</div>
 
 @endsection
